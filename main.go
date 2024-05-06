b313b11280a3f79d9570c63a5a85530096a574ce
Harel Safra <harel.safra@riskified.com>
2024-05-06 22:41:59 +0300

    Add check on empty result in database resource read. This enables correct handling in case the database was deleted outside TF

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"terraform-provider-neo4j/neo4j"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -8,9 +9,16 @@ import (
 )
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return neo4j.Provider()
 		},
+		ProviderAddr: "registry.terraform.io/headyj/neo4j",
+		Debug:        debug,
 	})
 }
